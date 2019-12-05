08612a99635602000ccb4f84baaf01e513039e29
Maxx Persin <maxxpersin@gmail.com>
2019-12-05 14:00:27 -0600

    working on js

---
+++ countries.js
@@ -0,0 +1,14 @@
+
+// console.log(submit);
+
+let setEvents = () => {
+
+}
+
+function search(evt) {
+    let submit = document.getElementById("search-form").lastChild;
+    //submit.onclick = (evt) => {
+    let input = submit.parentNode.firstChild;
+    console.log(input.value);
+    // }
+}
