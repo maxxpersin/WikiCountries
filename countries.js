265a0a62b2d89e2b138d58007ba6e5b64a8d7800
Maxx Persin <maxxpersin@gmail.com>
2019-12-06 12:46:26 -0600

    certain ajax get requests working

--- countries.js
+++ countries.js
@@ -1,19 +1,53 @@
 function addDropDown(obj) {
+    let container = document.getElementById('search');
 
+    if (document.getElementById('country-choices')) {
+        container.removeChild(document.getElementById('country-choices'));
+    }
+
+    let dropDown = document.createElement("select");
+    dropDown.id = 'country-choices';
+    container.appendChild(dropDown);
+    
+    obj.forEach(country => {
+        let item = document.createElement('option');
+        item.value = country.name;
+        let text = document.createTextNode(country.name);
+        item.appendChild(text);
+        dropDown.appendChild(item);
+    });
 }
 
 function search(evt) {
     let query = document.getElementById('input').value;
-    //if (query)
-    $.ajax({
-        url: 'https://restcountries.eu/rest/v2/alpha',
-        method: 'GET',
-        data: {alpha3code: query},
-        success: function(obj) {
-            console.log(obj);
-            //addDropDown(obj);
-        }
-    });
+    if (query.length === 2) {
+        $.ajax({
+            url: 'https://restcountries.eu/rest/v2/name/' + query,
+            method: 'GET',
+            data: {
+                // alpha2code: query,
+                fields: 'name;alpha2code'
+            },
+            success: function (obj) {
+                console.log(obj);
+                addDropDown(obj);
+            }
+        });
+    } else {
+        $.ajax({
+            url: 'https://restcountries.eu/rest/v2/name/' + query,
+            method: 'GET',
+            data: {
+                //alpha3code: query.substring(0, 3),
+                fields: 'name;alpha3code'
+            },
+            success: function (obj) {
+                console.log(obj);
+                addDropDown(obj);
+            }
+        });
+    }
+
 }
 
 var setEvents = function () {
