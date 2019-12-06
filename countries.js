cb6d18109c4c3dcdac390e7dda1a8ba1bcf0843a
Maxx Persin <36431722+maxxpersin@users.noreply.github.com>
2019-12-06 12:17:39 -0600

    implementing ajax requests

--- countries.js
+++ countries.js
@@ -1,14 +1,25 @@
-
-// console.log(submit);
-
-let setEvents = () => {
+function addDropDown(obj) {
 
 }
 
 function search(evt) {
-    let submit = document.getElementById("search-form").lastChild;
-    //submit.onclick = (evt) => {
-    let input = submit.parentNode.firstChild;
-    console.log(input.value);
-    // }
+    let query = document.getElementById('input').value;
+    //if (query)
+    $.ajax({
+        url: 'https://restcountries.eu/rest/v2/alpha',
+        method: 'GET',
+        data: {alpha3code: query},
+        success: function(obj) {
+            console.log(obj);
+            //addDropDown(obj);
+        }
+    });
 }
+
+var setEvents = function () {
+    let submit = document.getElementById('search-btn')
+    console.log(submit);
+    submit.addEventListener('click', search);
+    //submit.onclick = search(evt);
+}
+
